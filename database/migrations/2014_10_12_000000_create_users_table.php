3f95eda96d8b643afd6da493c7654451ea2c808e
IvonRobledo <ivon.robledo@hotmail.com>
2017-05-26 23:00:52 -0500

    gui-test

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,18 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-
- 
-
-
-            $table->string('name', 50);
-            $table->string('email', 50);
+            $table->string('name');
+            $table->string('email')->unique();
             $table->string('password');
-            $table->date('fechaNac');
-            $table->enum('genero', array('hombre', 'mujer'));
-            $table->text('biografia')->nullable();
-            $table->string('nombreFotoP')->nullable();
-            $table->string('extFotoP')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
